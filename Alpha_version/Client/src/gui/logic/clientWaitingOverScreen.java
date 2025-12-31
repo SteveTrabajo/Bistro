7cb6eceebe696027784aeceb6bdb8eadc7bc9d35
Uriel Julian Pekelis <urielpekelis@gmail.com>
2025-12-31 14:39:10 +0200

    Refactor icon paths and add clientWaitingOverScreen logic
    Updated FXML files to use new icon paths under resources/icons and adjusted stylesheet references for consistency. Added clientWaitingOverScreen.java logic controller. Renamed and added several icon files to match new naming conventions and UI requirements. Removed unused legend entries for 'Fully Booked' in booking screens.

---
+++ Alpha_version/Client/src/gui/logic/clientWaitingOverScreen.java
@@ -0,0 +1,27 @@
+package gui.logic;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import logic.BistroClientGUI;
+
+public class clientWaitingOverScreen {
+
+	@FXML
+	private Button btnFinish;
+	@FXML
+	private Label lblTableNum;
+	
+	@FXML
+	public void initialize() {
+		int tableNum = BistroClientGUI.client.getTableCTRL().getUserAllocatedTable();
+		lblTableNum.setText("Your table number is: " + tableNum);
+	}
+	
+	@FXML
+	void btnFinish(Event event) {
+	    System.out.println("Finish button clicked in clientWaitingOverScreen.");
+	    BistroClientGUI.switchScreen(event, "ClientDashboardScreen", "Error returning to Client Dashboard Screen.");
+	}
+}
